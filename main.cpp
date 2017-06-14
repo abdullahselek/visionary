6aa213741f594fd9f1f7a4aaeac19e9a85a0132e
Abdullah Selek <abdullahselek@gmail.com>
2017-06-14 16:58:34 +0200

    add run arguments

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include "src/motion-detector.h"
+#include "src/face-detector.h"
+#include "test/utils.h"
 
-int main() {
-    MotionDetector motionDetector;
-    motionDetector.setCeil(15);
-    motionDetector.openCamera();
-    motionDetector.run();
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        return 0;
+    } else {
+        std::string run = argv[1];
+        if (run == "motion") {
+            MotionDetector motionDetector;
+            motionDetector.setCeil(15);
+            motionDetector.openCamera();
+            motionDetector.run();
+        } else  {
+            const char *currentPath = Utils::currentDirectory();
+            const char *cascadePath = "/haarcascades/haarcascade_frontalface_alt.xml";
+            std::string fullCascadePath(currentPath);
+            Utils::replaceStringInPlace(fullCascadePath, "/src", "");
+            fullCascadePath += cascadePath;
+
+            FaceDetector faceDetector(fullCascadePath.c_str(), nullptr);
+            faceDetector.run();
+        }
+    }
 
     return 0;
 }
