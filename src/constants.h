973e9d5f968f9c0174af35c11154a85016fa3da5
abdullahselek <abdullahselek@gmail.com>
2017-06-20 19:23:01 +0200

    create tracker types

--- src/constants.h
+++ src/constants.h
@@ -6,19 +6,27 @@
 #define VISIONARY_CONSTANTS_H
 
 namespace window {
-
     const std::string kTarget = "Target";
-
 }
 
 namespace source {
-
     enum type {
         image,
         video,
         camera
     };
+}
 
+namespace tracker {
+    enum type {
+        MIL,
+        BOOSTING
+    };
+
+    static const char * TrackerValues[] = { "MIL", "BOOSTING" };
+    const char * getCharForEnum(int enumVal) {
+        return TrackerValues[enumVal];
+    }
 }
 
 #endif //VISIONARY_CONSTANTS_H
