6c36040c7d13f3defe290985454d5199dac23a0d
Abdullah Selek <abdullahselek@gmail.com>
2017-06-23 09:11:06 +0200

    source bash profile

--- .ci/installOnOSX.sh
+++ .ci/installOnOSX.sh
@@ -7,8 +7,5 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     rm -rf /usr/local/bin/f2py
     rm -rf /usr/local/lib/python2.7
     brew install opencv3 --with-contrib --with-ffmpeg
-    echo 'export PATH="/usr/local/opt/opencv3/bin:$PATH"' >> ~/.bash_profile
-    source ~/.bash_profile
-    cat ~/.bash_profile
 
 fi
